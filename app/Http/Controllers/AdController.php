5b071571a7648adadc38814a872734c52d2554ef
Samuel Felipe <samfelgar@gmail.com>
2020-09-08 19:06:26 -0300

    view prototyping

---
+++ app/Http/Controllers/AdController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdController extends Controller
+{
+    public function show()
+    {
+        return view('ad');
+    }
+}
